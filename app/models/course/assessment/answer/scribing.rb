4de4ba2a2fc70f791eb73ff893e4e040af4135d3
Nicholette <nekonekonik@gmail.com>
2017-03-15 10:51:34 +0800

    Midpoint save for answer model

---
+++ app/models/course/assessment/answer/scribing.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+class Course::Assessment::Answer::Scribing < ActiveRecord::Base
+  # # The table name for this model is singular.
+  # self.table_name = table_name.singularize
+
+  acts_as :answer, class_name: Course::Assessment::Answer.name
+
+  # has_one :document_file, class_name: Course::Assessment::Answer::ScribingDocumentFile.name,
+  #         dependent: :destroy, foreign_key: :answer_id, inverse_of: :answer
+
+
+  # accepts_nested_attributes_for :files, allow_destroy: true
+
+  def to_partial_path
+    'course/assessment/answer/scribing/scribing'.freeze
+  end
+
+  # Specific implementation of Course::Assessment::Answer#reset_answer
+  # def reset_answer
+  #   self.class.transaction do
+  #     files.clear
+  #     question.specific.copy_template_files_to(self)
+  #     raise ActiveRecord::Rollback unless save
+  #   end
+  #   acting_as
+  # end
+
+  MAX_ATTEMPTING_TIMES = 1000
+  # Returns the attempting times left for current answer.
+  # The max attempting times will be returned if question don't have the limit.
+  #
+  # @return [Integer]
+  def attempting_times_left
+    @times_left ||= begin
+      return MAX_ATTEMPTING_TIMES unless question.actable.attempt_limit
+
+      times = question.actable.attempt_limit - submission.evaluated_or_graded_answers(question).size
+      times = 0 if times < 0
+      times
+    end
+  end
+end
--- app/models/course/assessment/question/scribing.rb
+++ app/models/course/assessment/question/scribing.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 class Course::Assessment::Question::Scribing < ActiveRecord::Base
+
+  # # The table name for this model is singular.
+  # self.table_name = table_name.singularize
+
   acts_as :question, class_name: Course::Assessment::Question.name
 
-  has_one :document_file, class_name: Course::Assessment::Question::ScribingDocumentFile.name,
-          dependent: :destroy, foreign_key: :question_id, inverse_of: :question
+  # has_one :document_file, class_name: Course::Assessment::Question::ScribingDocumentFile.name,
+  #         dependent: :destroy, foreign_key: :question_id, inverse_of: :question
+
+  def to_partial_path
+    'course/assessment/question/scribing/scribing'.freeze
+  end
+
+  def attempt(submission, last_attempt = nil)
+    answer = submission.scribing_answers.build(submission: submission, question: question)
+    if last_attempt
+      answer.scribe = last_attempt.scribe
+    end
+    answer.acting_as
+  end
 end

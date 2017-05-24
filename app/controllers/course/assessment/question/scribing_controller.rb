dab38fbe484d40c2b0b9cc67ee2d2f8e5e0c8339
Nicholette <nekonekonik@gmail.com>
2017-05-24 14:20:24 +0800

    wip: scribing question creator service

--- app/controllers/course/assessment/question/scribing_controller.rb
+++ app/controllers/course/assessment/question/scribing_controller.rb
@@ -20,24 +20,13 @@ class Course::Assessment::Question::ScribingController < \
     end
   end
 
-  def create # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+  def create
     # special handling for PDF files
-    if params&.[](:question_scribing)&.[](:file)&.content_type&.downcase == 'application/pdf'
-      file = params[:question_scribing][:file]
-      respond_to do |format|
-        if generate_pdf_questions(file)
-          format.json { render_success_json t('.success') }
-        else
-          format.json { render_failure_json t('.failure') }
-        end
-      end
-    else
-      respond_to do |format|
-        if @scribing_question.save
-          format.json { render_scribing_question_json }
-        else
-          format.json { render_failure_json t('.failure') }
-        end
+    respond_to do |format|
+      if file_for_uploading? && scribing_question_service(scribing_question_params[:file]).save_question
+        format.json { render_success_json t('.success') }
+      else
+        format.json { render_failure_json t('.failure') }
       end
     end
   end
@@ -50,8 +39,9 @@ class Course::Assessment::Question::ScribingController < \
   end
 
   def update
+    # TODO: Handle updating of attachments in scribing question
     # Don't allow the user to edit attachment at the moment
-    # In case the user uploads a multipage pdf to replace
+    # in case the user uploads a multipage pdf to replace
     respond_to do |format|
       if @scribing_question.update(scribing_question_update_params)
         format.json { render_scribing_question_json }
@@ -103,53 +93,16 @@ class Course::Assessment::Question::ScribingController < \
            status: :bad_request
   end
 
-  def generate_pdf_questions(file) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-    @scribing_question.class.transaction do # rubocop:disable Metrics/BlockLength
-      filename = remove_whitespace(File.basename(file.original_filename, '.pdf'))
-      pdf_file = MiniMagick::Image.new(file.tempfile.path)
-
-      pdf_file.pages.each_with_index do |_page, index|
-        # Converts multipage pdf to single page pdf, then convert them with density
-        temp_filename = "#{filename}[#{index + 1}].png"
-
-        image = MiniMagick::Image.new(file.tempfile.path + "[#{index}]")
-        MiniMagick::Tool::Convert.new do |convert|
-          convert.render
-          convert.density(300)
-          # need to check whether to resize image first or later
-          convert.background('white')
-          convert.flatten
-          convert << image.path
-          convert << temp_filename
-        end
-
-        new_question = Course::Assessment::Question::Scribing.new
-
-        # Leave filename sanitization to attachment reference
-        fake_upload_file = ActionDispatch::Http::UploadedFile.new(tempfile: File.new(temp_filename),
-                                                                  filename: temp_filename.dup,
-                                                                  type: 'image/png')
-        new_question.file = fake_upload_file
-        new_question.title = @scribing_question.title
-        new_question.description = @scribing_question.description
-        new_question.maximum_grade = @scribing_question.maximum_grade
-        new_question.creator = current_user
-        new_question.assessment = @assessment
-
-        # Make sure new questions appear at the end of the list.
-        max_weight = @assessment.questions.pluck(:weight).max
-        new_question.weight ||= max_weight ? max_weight + 1 : 0
-
-        raise ActiveRecord::Rollback unless new_question.save
-      end
-      true
-    end
+  # Check whether the file types are allowed
+  def file_for_uploading?
+    allowed_file_types = ['image/gif', 'image/png', 'image/jpeg', 'image/pjpeg', 'application/pdf']
+    allowed_file_types.include? scribing_question_params.dig(:file)&.content_type&.downcase
   end
 
-  # Convert spaces to underscore
-  def remove_whitespace(filename)
-    filename.tap do |name|
-      name.tr! ' ', '_'
-    end
+  def scribing_question_service(file)
+    max_weight = @assessment.questions.pluck(:weight).max
+    @service ||= Course::Assessment::Question::Scribing::ScribingQuestionService.new(
+      @scribing_question, file, current_user, @assessment
+    )
   end
 end

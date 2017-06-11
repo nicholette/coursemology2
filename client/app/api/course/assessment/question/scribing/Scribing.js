2656eb3c1d3ea291873d3a78ed9ef437d2ab797c
Nicholette <nekonekonik@gmail.com>
2017-06-11 04:18:37 -0700

    Add scribing question react client

---
+++ client/app/api/course/assessment/question/scribing/Scribing.js
@@ -0,0 +1,100 @@
+import BaseScribingAPI from './Base';
+
+export default class ScribingsAPI extends BaseScribingAPI {
+  /**
+   * scribing_question = {
+  *   id: number,
+  *   title: string,
+  *   description: string,
+  *   staff_only_comments: string,
+  *   maximum_grade: string,
+  *   weight: number,
+  *   skill_ids [],
+  *   skills: [],
+  *   published_assessment: boolean,
+  *   attempt_limit: number,
+  * }
+   */
+
+  /**
+   * Fetches a Scribing question
+   *
+   * @param {number} scribingId
+   * @return {Promise}
+   * success response: scribing_question
+   */
+  fetch(scribingId) {
+    return this.getClient().get(`${this._getUrlPrefix()}/${scribingId}`);
+  }
+
+  /**
+   * Creates a Scribing question
+   *
+   * @param {object} scribingFields - params in the format of
+   *                                { question_scribing: { :title, :description, etc } }
+   * @return {Promise}
+   * success response: scribing_question
+   * error response: { errors: [{ attribute: string }] }
+   */
+  create(scribingFields) {
+    const config = { 
+      headers: { 
+        'Content-Type': 'multipart/form-data',
+        'Accept': 'file_types',
+      }
+    };
+    let innerFormData = new FormData();
+
+    for (var key in scribingFields.question_scribing) {
+      if (scribingFields.question_scribing.hasOwnProperty(key)) {
+        innerFormData.append(`question_scribing[${key}]`, scribingFields.question_scribing[key]);
+      }
+    }
+
+    return this.getClient().post(this._getUrlPrefix(), innerFormData, config);
+  }
+
+  /**
+   * Updates a Scribing question
+   *
+   * @param {number} scribingId
+   * @param {object} scribingFields - params in the format of
+   *                                { survey: { :title, :description, etc } }
+   * @return {Promise}
+   * success response: scribing_question
+   * error response: { errors: [{ attribute: string }] }
+   */
+  update(scribingId, scribingFields) {
+    const config = { 
+      headers: { 
+        'Content-Type': 'multipart/form-data',
+        'Accept': 'file_types'
+      }
+    };
+    let innerFormData = new FormData();
+
+    for (var key in scribingFields.question_scribing) {
+      if (scribingFields.question_scribing.hasOwnProperty(key)) {
+        innerFormData.append(`question_scribing[${key}]`, scribingFields.question_scribing[key]);
+      }
+    }
+
+    return this.getClient().patch(`${this._getUrlPrefix()}/${scribingId}`, innerFormData, config);
+  }
+
+  /**
+   * Deletes a Scribing question
+   *
+   * @param {number} scribingId
+   * @return {Promise}
+   * success response: {}
+   * error response: {}
+   */
+  delete(scribingId) {
+    return this.getClient().delete(`${this._getUrlPrefix()}/${scribingId}`);
+  }
+
+  _getUrlPrefix() {
+    return `/courses/${this.getCourseId()}/assessments/${this.getAssessmentId()}/question/scribing`;
+  }
+}

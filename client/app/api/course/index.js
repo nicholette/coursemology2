20e0a501f92348fbb718cbe378b524f44aece597
Nicholette <nekonekonik@gmail.com>
2017-06-11 04:18:37 -0700

    Add scribing question react client

--- client/app/api/course/index.js
+++ client/app/api/course/index.js
@@ -4,6 +4,7 @@ import MaterialsAPI from './Materials';
 import MaterialFoldersAPI from './MaterialFolders';
 import LessonPlanAPI from './LessonPlan';
 import SurveyAPI from './Survey';
+import ScribingQuestionAPI from './assessment/question/scribing';
 
 const CourseAPI = {
   assessments: new AssessmentsAPI(),
@@ -12,6 +13,9 @@ const CourseAPI = {
   materialFolders: new MaterialFoldersAPI(),
   lessonPlan: new LessonPlanAPI(),
   survey: SurveyAPI,
+  question: {
+    scribing: ScribingQuestionAPI,
+  },
 };
 
 Object.freeze(CourseAPI);
---
+++ client/app/bundles/course/assessment/question/scribing/components/SummernoteField.jsx
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react';
+import { Field } from 'redux-form';
+import MaterialSummernote from 'lib/components/redux-form/RichTextField';
+import { questionNamePrefix, questionIdPrefix } from '../constants';
+
+const propTypes = {
+  label: PropTypes.string.isRequired,
+  field: PropTypes.string.isRequired,
+  validate: PropTypes.array,
+  is_loading: PropTypes.bool,
+}
+
+export default class SummernoteField extends Component {
+  render() {
+    const { label, field, validate } = this.props;
+    return (
+       <Field
+        name={ questionNamePrefix + field }
+        id={ questionIdPrefix + field}
+        label={ label }
+        validate={validate}
+        component={MaterialSummernote}
+      />
+    );
+  }
+}
+
+SummernoteField.propTypes = propTypes;
